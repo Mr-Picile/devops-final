f714af45e7a155e6631e2294e0c36af0b7effc46
Peter Plozay <61207550+Mr-Picile@users.noreply.github.com>
2026-04-28 01:56:20 -0500

    Update main.tf

--- stage2/main.tf
+++ stage2/main.tf
@@ -1,1 +1,61 @@
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "pplozay-ACR" {
+  name = "pplozayacmp2400"
+  resource_group_name = "rg-pplozay"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
+#stage2 start here
+resource "azurerm_container_group" "pplozay-aci" {
+  name                = "acmp-pplozay-aci"
+  location            = "Central US"
+  resource_group_name = "rg-pplozay"
+  ip_address_type     = "Public"
+  dns_name_label      = "acmp-pplozay-instance"
+  os_type             = "Linux"
+
+  container {
+    name   = "final"
+    image  = "pplozayacmp2400.azurecr.io/final:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 8000
+      protocol = "TCP"
+    }
+  
+    secure_environment_variables = {
+      DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+    }
+  }
+  image_registry_credential {
+    server = "pplozayacmp2400.azurecr.io"
+    username = var.ARM_CLIENT_ID
+    password = var.ARM_CLIENT_SECRET
+  }
+  }
 
